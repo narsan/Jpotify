913c93f3217bd6fd6e0164ea9f4df881efe02ffa
shakibaam <shakibaamirshahi@yahoo.com>
2019-06-23 12:19:43 +0330

    test

--- src/ShowSongs.java
+++ src/ShowSongs.java
@@ -51,160 +51,149 @@ public class ShowSongs extends JButton {
 //            Iterator iterator=Library.getSongs().iterator();
         for (int i = 0; i < Library.getSongs().size(); i++) {
             File temp = Library.getSongs().get(i);
-            Mp3File mp3File = new Mp3File(temp);
+            try {
 
-            if (mp3File.hasId3v1Tag()&&mp3File.getId3v1Tag().getTitle()!=null) {
-                ID3v1 id3v1 = mp3File.getId3v1Tag();
-                Title = new JLabel();
-                Title.setVisible(true);
-
-                Title.setText(id3v1.getTitle());
+                Mp3File mp3File = new Mp3File(temp);
 
 
+                if (mp3File.hasId3v1Tag() && mp3File.getId3v1Tag().getTitle() != null) {
+                    ID3v1 id3v1 = mp3File.getId3v1Tag();
+                    Title = new JLabel();
+                    Title.setVisible(true);
 
-                Title.setFont(new Font("Arial", Font.PLAIN, 13));
-                Title.setForeground(Color.white);
-            }
-
-            else if (mp3File.hasId3v2Tag()&&mp3File.getId3v2Tag().getTitle()!=null){
-
-                ID3v2 id3v2 = mp3File.getId3v2Tag();
-                Title = new JLabel();
-                Title.setVisible(true);
-
-                Title.setText(id3v2.getTitle());
-
-                Title.setFont(new Font("Arial", Font.PLAIN, 13));
-                Title.setForeground(Color.white);
-
-            }
+                    Title.setText(id3v1.getTitle());
 
 
+                    Title.setFont(new Font("Arial", Font.PLAIN, 13));
+                    Title.setForeground(Color.white);
+                } else if (mp3File.hasId3v2Tag() && mp3File.getId3v2Tag().getTitle() != null) {
 
-            if (mp3File.hasId3v2Tag()) {
+                    ID3v2 id3v2 = mp3File.getId3v2Tag();
+                    Title = new JLabel();
+                    Title.setVisible(true);
 
+                    Title.setText(id3v2.getTitle());
 
-                ID3v2 id3v2 = mp3File.getId3v2Tag();
-                ID3v1 id3v1=mp3File.getId3v1Tag();
-                songImage = new JButton();
-                songImage.setBorder(null);
-                songImage.setVisible(true);
+                    Title.setFont(new Font("Arial", Font.PLAIN, 13));
+                    Title.setForeground(Color.white);
 
-
-                songImage.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JPanel showPlayingSong = new JPanel();
-                        JPanel cuurentPanel=null;
-                        showPlayingSong.setLayout(new BorderLayout());
-                        showPlayingSong.setBackground(Color.black);
-                        byte[] imageData = id3v2.getAlbumImage();
-                        BufferedImage img = null;
-                        try {
-                            img = ImageIO.read(new ByteArrayInputStream(imageData));
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
-
-                        ImageIcon imageIcon = new ImageIcon(img.getScaledInstance(80, 80, Image.SCALE_DEFAULT));
-
-                        showPlayingSong.add(new JLabel(imageIcon), BorderLayout.WEST);
-                        if(id3v2.getArtist()!=null){
-                        JLabel Artist = new JLabel(id3v2.getArtist());
-                            Artist.setForeground(Color.WHITE);
-                            showPlayingSong.add(Artist, BorderLayout.PAGE_END);
-                        }
-
-                        else if (mp3File.hasId3v1Tag()&&mp3File.getId3v1Tag().getArtist()!=null){
-
-                            JLabel Artist = new JLabel(id3v1.getArtist());
-                            Artist.setForeground(Color.WHITE);
-                            showPlayingSong.add(Artist, BorderLayout.PAGE_END);
-
-                        }
-
-                        if(id3v2.getTitle()!=null){
-
-                            JLabel Title = new JLabel(id3v2.getTitle());
-                            Title.setForeground(Color.WHITE);
-                            showPlayingSong.add(Title, BorderLayout.CENTER);
-                        }
-
-                        else if(id3v1.getAlbum()!=null){
-
-                            JLabel Title = new JLabel(id3v1.getTitle());
-                            Title.setForeground(Color.WHITE);
-                            showPlayingSong.add(Title, BorderLayout.CENTER);
-
-                        }
-
-                        if (id3v2.getAlbum()!=null){
+                }
 
 
-                            JLabel album = new JLabel(id3v2.getAlbum());
-                            album.setForeground(Color.WHITE);
-                            showPlayingSong.add(album, BorderLayout.NORTH);
-                        }
+                if (mp3File.hasId3v2Tag()) {
 
-                        else if (id3v1.getAlbum()!=null){
-                            JLabel album = new JLabel(id3v1.getAlbum());
-                            album.setForeground(Color.WHITE);
-                            showPlayingSong.add(album, BorderLayout.NORTH);
 
-                        }
-                        showPlayingSong.setBackground(new Color(58,58,58));
-                        showPlayingSong.setPreferredSize(new Dimension(318,0));
+                    ID3v2 id3v2 = mp3File.getId3v2Tag();
+                    ID3v1 id3v1 = mp3File.getId3v1Tag();
+                    songImage = new JButton();
+                    songImage.setBorder(null);
+                    songImage.setVisible(true);
 
-                        FileInputStream in= null;
-                        try {
-                            in = new FileInputStream(temp);
-                        } catch (FileNotFoundException e1) {
-                            e1.printStackTrace();
-                        }
 
-                        PausablePlayer player= null;
-                        try {
-                            player = new PausablePlayer(in);
-                        } catch (JavaLayerException e1) {
-                            e1.printStackTrace();
-                        }
+                    songImage.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            JPanel showPlayingSong = new JPanel();
+                            JPanel cuurentPanel = null;
+                            showPlayingSong.setLayout(new BorderLayout());
+                            showPlayingSong.setBackground(Color.black);
+                            byte[] imageData = id3v2.getAlbumImage();
+                            BufferedImage img = null;
+                            try {
+                                img = ImageIO.read(new ByteArrayInputStream(imageData));
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+
+                            ImageIcon imageIcon = new ImageIcon(img.getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+
+                            showPlayingSong.add(new JLabel(imageIcon), BorderLayout.WEST);
+                            if (id3v2.getArtist() != null) {
+                                JLabel Artist = new JLabel(id3v2.getArtist());
+                                Artist.setForeground(Color.WHITE);
+                                showPlayingSong.add(Artist, BorderLayout.PAGE_END);
+                            } else if (mp3File.hasId3v1Tag() && mp3File.getId3v1Tag().getArtist() != null) {
+
+                                JLabel Artist = new JLabel(id3v1.getArtist());
+                                Artist.setForeground(Color.WHITE);
+                                showPlayingSong.add(Artist, BorderLayout.PAGE_END);
+
+                            }
+
+                            if (id3v2.getTitle() != null) {
+
+                                JLabel Title = new JLabel(id3v2.getTitle());
+                                Title.setForeground(Color.WHITE);
+                                showPlayingSong.add(Title, BorderLayout.CENTER);
+                            } else if (id3v1.getAlbum() != null) {
+
+                                JLabel Title = new JLabel(id3v1.getTitle());
+                                Title.setForeground(Color.WHITE);
+                                showPlayingSong.add(Title, BorderLayout.CENTER);
+
+                            }
+
+                            if (id3v2.getAlbum() != null) {
+
+
+                                JLabel album = new JLabel(id3v2.getAlbum());
+                                album.setForeground(Color.WHITE);
+                                showPlayingSong.add(album, BorderLayout.NORTH);
+                            } else if (id3v1.getAlbum() != null) {
+                                JLabel album = new JLabel(id3v1.getAlbum());
+                                album.setForeground(Color.WHITE);
+                                showPlayingSong.add(album, BorderLayout.NORTH);
+
+                            }
+                            showPlayingSong.setBackground(new Color(58, 58, 58));
+                            showPlayingSong.setPreferredSize(new Dimension(318, 0));
+
+                            FileInputStream in = null;
+                            try {
+                                in = new FileInputStream(temp);
+                            } catch (FileNotFoundException e1) {
+                                e1.printStackTrace();
+                            }
+
+                            PausablePlayer player = null;
+                            try {
+                                player = new PausablePlayer(in);
+                            } catch (JavaLayerException e1) {
+                                e1.printStackTrace();
+                            }
 
 
                             Playing.setFile(temp);
                             Playing.setPlayer(player);
                             Playing.plaiyingSongs.add(player);
-                            PlayMusic playMusic1=new PlayMusic(temp,player);
-                        try {
-                            Playing.Play();
-                        } catch (JavaLayerException e1) {
-                            e1.printStackTrace();
-                        }
+                            PlayMusic playMusic1 = new PlayMusic(temp, player);
+                            try {
+                                Playing.Play();
+                            } catch (JavaLayerException e1) {
+                                e1.printStackTrace();
+                            }
 
-                        if (cuurentPanel!=null){
+                            if (cuurentPanel != null) {
 
-                            cuurentPanel.setVisible(false);
-                            DownPanel.downPanel.remove(cuurentPanel);
-                            cuurentPanel=showPlayingSong;
-                            DownPanel.addPlayingSongInfo(showPlayingSong);
-                            DownPanel.downPanel.revalidate();
-                        }
+                                cuurentPanel.setVisible(false);
+                                DownPanel.downPanel.remove(cuurentPanel);
+                                cuurentPanel = showPlayingSong;
+                                DownPanel.addPlayingSongInfo(showPlayingSong);
+                                DownPanel.downPanel.revalidate();
+                            } else {
 
-                        else {
+                                cuurentPanel = showPlayingSong;
 
-                            cuurentPanel=showPlayingSong;
+                                DownPanel.addPlayingSongInfo(showPlayingSong);
+                                DownPanel.downPanel.revalidate();
 
-                            DownPanel.addPlayingSongInfo(showPlayingSong);
-                            DownPanel.downPanel.revalidate();
-
-                        }
-
-
+                            }
 
 
                             DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
                             Date date = new Date();
-                            File file=Library.file;
-                            PrintWriter writer= null;
+                            File file = Library.file;
+                            PrintWriter writer = null;
                             try {
                                 writer = new PrintWriter(file);
                             } catch (FileNotFoundException e1) {
@@ -218,23 +207,30 @@ public class ShowSongs extends JButton {
                             e1.printStackTrace();
                         }*/
 
-                        writer.println(dateFormat.format(date)+"  "+temp);
+                            writer.println(dateFormat.format(date) + "  " + temp);
 
                             playedSongs.add(player);
 
 
+                        }
+                    });
 
-                    }
-                });
+                    byte[] imageData = id3v2.getAlbumImage();
+                    BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageData));
+                    ImageIcon imageIcon = new ImageIcon(img.getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+                    songImage.setIcon(imageIcon);
+                    String mimeType = id3v2.getAlbumImageMimeType();
+                    System.out.println(mimeType);
+                    Title.setText(id3v2.getTitle());
 
-                byte[] imageData = id3v2.getAlbumImage();
-                BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageData));
-                ImageIcon imageIcon = new ImageIcon(img.getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-                songImage.setIcon(imageIcon);
-                String mimeType = id3v2.getAlbumImageMimeType();
-                System.out.println(mimeType);
-                Title.setText(id3v2.getTitle());
+                }
 
+            }
+
+
+            catch(IOException e){
+
+                System.out.println("exception");
             }
 
 
